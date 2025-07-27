812ad42dda458b2bce77384a1c72bde3ebba14d7
thisioliverthings <Abodeking19999@gmail.com>
2025-07-27 10:45:05 +0300

    Update language.py

--- LunaMusic/utils/decorators/language.py
+++ LunaMusic/utils/decorators/language.py
@@ -9,8 +9,8 @@
 
 
 from strings import get_string
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_commanddelete_on,
+from LunaMusic.misc import SUDOERS
+from LunaMusic.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
