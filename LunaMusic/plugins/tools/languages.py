bce9258bace0de3adc79821984a3c3b6c921fa60
thisioliverthings <Abodeking19999@gmail.com>
2025-07-27 15:42:15 +0300

    Update languages.py

--- LunaMusic/plugins/tools/languages.py
+++ LunaMusic/plugins/tools/languages.py
@@ -13,9 +13,9 @@ from pyrogram.types import InlineKeyboardButton, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string, languages_present
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_lang, set_lang
-from YukkiMusic.utils.decorators import (ActualAdminCB, language,
+from LunaMusic import app
+from LunaMusic.utils.database import get_lang, set_lang
+from LunaMusic.utils.decorators import (ActualAdminCB, language,
                                          languageCB)
 
 # Languages Available
