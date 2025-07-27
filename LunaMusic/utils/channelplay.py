bff12fbb1e41ca44da8cc8158e1b705ec4ddb054
thisioliverthings <Abodeking19999@gmail.com>
2025-07-27 10:40:22 +0300

    Update channelplay.py

--- LunaMusic/utils/channelplay.py
+++ LunaMusic/utils/channelplay.py
@@ -7,8 +7,8 @@
 #
 # All rights reserved.
 
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_cmode
+from LunaMusic import app
+from LunaMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
