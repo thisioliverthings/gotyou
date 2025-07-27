3572b174530b6381f9baf0bce97c04bc1f01dee5
thisioliverthings <Abodeking19999@gmail.com>
2025-07-27 10:47:32 +0300

    Update assistantdatabase.py

--- LunaMusic/utils/database/assistantdatabase.py
+++ LunaMusic/utils/database/assistantdatabase.py
@@ -45,7 +45,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from YukkiMusic.core.userbot import assistants
+    from LunaMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -72,7 +72,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from YukkiMusic.core.userbot import assistants
+    from LunaMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -85,7 +85,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from YukkiMusic.core.userbot import assistants
+    from LunaMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
